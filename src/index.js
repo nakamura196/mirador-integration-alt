8a93e1390c3d6d4dd3375ca95c4b26f5d366d582
nakamura196 <na.kamura.1263@gmail.com>
2022-06-08 15:37:37 +0900

    feat: minor update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import Mirador from 'mirador/dist/es/src/index';
 import { miradorImageToolsPlugin } from 'mirador-image-tools';
 
+
 const integration = {
   Mirador,
   miradorImageToolsPlugin
 }
 
-export default {
-  ...integration
-}
+export default integration
