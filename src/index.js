c8de98e25ec34efcc4db5b5ea29e09a70db237dd
Satoru <you@example.com>
2023-07-08 08:42:49 +0900

    feat: add MiradorCopyWindowPlugin

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import Mirador from 'mirador/dist/es/src/index';
 import { miradorImageToolsPlugin } from 'mirador-image-tools';
+import { MiradorCopyWindowPlugin } from 'mirador-copy-window-plugin';
 
 
 const integration = {
   Mirador,
-  miradorImageToolsPlugin
+  miradorImageToolsPlugin,
+  MiradorCopyWindowPlugin
 }
 
 export default integration
